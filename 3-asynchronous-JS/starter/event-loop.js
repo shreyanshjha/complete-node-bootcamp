9e08a9d65160c304cc87fa76ebe918f7c0350ac2
shreyanshjha <shreyanshjha22@gmail.com>
2024-12-22 23:58:02 +0530

    event loop nodejs behind the scenes

---
+++ 3-asynchronous-JS/starter/event-loop.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const start = Date.now();
+setTimeout(() => console.log("Timer 1 finished"), 0);
+
+setImmediate(() => console.log("Immediate 1 finished"));
+
+process.env.UV_THREADPOOL_SIZE = 50;
+
+fs.readFile('dog.txt', () => {
+    console.log("I/O finished");
+    console.log("--------------------------------");
+
+    setTimeout(() => console.log("Timer 2 finished"), 0);
+    setTimeout(() => console.log("Timer 3 finished"), 3000);
+
+    setImmediate(() => console.log("Immediate 2 finished"));
+
+    process.nextTick(() => console.log("Process.nextTick"));
+
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,"Password encrypted");
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,"Password encrypted");
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,"Password encrypted");
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,"Password encrypted");
+    });
+});
+
+console.log("Hello from top-level code");
